79327870c9153ff7a727f45f73c1cc536b8261b0
Jack Kay <jackkay@Jacks-MacBook-Pro.local>
2022-08-21 00:16:55 -0700

    Toggle works, React Form, state and props update to new task, deleted hard coded array

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import { useState } from 'react'
-import Header from './components/Header'
-import Tasks from './components/Tasks'
+import { useState } from 'react';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 
-// headache 1:03
+
 function App() {
-const [tasks, setTasks] = useState([
-  {
-    id: 1,
-    text: 'Doctors Appoinment',
-    day: 'Feb 6',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Walk Dog',
-    day: 'Feb 2',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Meeting',
-    day: 'feb 9',
-    reminder: false,
-  },
-])
+  const [showAddTask, setShowAddTask] = useState(false)
+  const [tasks, setTasks] = useState([])
+
+const addTask = (task) => {
+  const id = Math.floor(Math.random() * 10000) + 1
+
+  const newTask = { id, ...task }
+  setTasks([...tasks, newTask])
+}
 
 // delete task from ui
 const deleteTask = (id) => {
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
-// reminder green notification
+//  toggle reminder green notification
 const toggleReminder = (id) => {
   setTasks(tasks.map((task) => 
   task.id === id ? { ...task, reminder: !task.reminder} : task))
 }
   return (
     <div className='container'>
-      <Header />
+      <Header
+      onAdd={() => setShowAddTask
+      (!showAddTask)} showAdd={showAddTask} />
+
+      {showAddTask && <AddTask onAdd={addTask} />}
+
       {tasks.length > 0 ? <Tasks tasks={tasks} 
       onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks to Show'}
     </div>  
